b8da724609261badfa057d335b63ca8501dcc321
Kuntech0 <kuntech07@gmail.com>
2025-10-31 10:41:52 +0530

    initial commit

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -1,35 +1,98 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+import GoalForm from "./pages/GoalForm";
+import Goals from "./pages/Goals"; 
+import SipGoals from "./pages/SipGoals"; 
+import LumpSumGoalDetail from "./pages/LumpSumGoalDetail";
+import SipGoalDetail from "./pages/SipGoalDetail";
+import Profile from "./pages/Profile";
+import SipCalculator from "./pages/SipCalculator";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/goal"
+          element={
+            <ProtectedRoute>
+              <GoalForm />
+            </ProtectedRoute>
+          }
+        />
+ <Route
+          path="/sip-calculator"
+          element={
+            <ProtectedRoute>
+              <SipCalculator />
+            </ProtectedRoute>
+          }
+        />
+
+ <Route
+          path="/goals/lumpsum/chart/:id"
+          element={
+            <ProtectedRoute>
+              <LumpSumGoalDetail />
+            </ProtectedRoute>
+          }
+        />
+
+         <Route
+          path="/goals/sip/chart/:id"
+          element={
+            <ProtectedRoute>
+              <SipGoalDetail />
+            </ProtectedRoute>
+          }
+        />
+
+
+ <Route
+          path="/sip-goals"
+          element={
+            <ProtectedRoute>
+              <SipGoals />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/lumpsum-goals"
+          element={
+            <ProtectedRoute>
+              <Goals />
+            </ProtectedRoute>
+          }
+        />
+
+
+         <Route
+          path="/profile"
+          element={
+            <ProtectedRoute>
+              <Profile />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
